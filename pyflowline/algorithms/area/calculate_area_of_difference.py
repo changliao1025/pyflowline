5dc9cca6b1093528a3057241ca60e14ae072fdc6
changliao1025 <changliao1025@outlook.com>
2022-01-15 13:32:31 -0800

    add area of diff

---
+++ pyflowline/algorithms/area/calculate_area_of_difference.py
@@ -0,0 +1,26 @@
+import os
+import json
+import numpy as np
+from osgeo import ogr, osr, gdal, gdalconst
+from shapely.geometry import Point, LineString, MultiLineString
+from shapely.wkt import loads
+
+from pyflowline.classes.edge import pyedge
+from pyflowline.classes.vertex import pyvertex
+from pyflowline.classes.flowline import pyflowline
+
+from pyflowline.formats.convert_coordinates import convert_gcs_coordinates_to_flowline, convert_pcs_coordinates_to_flowline
+
+def calculate_area_of_difference_raw(sFilename_a, sFilename_b):
+
+    
+
+
+    return
+
+def calculate_area_of_difference_simplified(sFilename_a, sFilename_b):
+
+    
+
+
+    return

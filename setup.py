49ca9e0e88eb93a185c85b67b42ae7ca7684ff69
changliao1025 <changliao1025@outlook.com>
2022-02-28 19:54:05 -0800

    up

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ DESCRIPTION = \
 AUTHOR = "Chang Liao"
 AUTHOR_EMAIL = "chang.liao@pnnl.gov"
 URL = "https://github.com/changliao1025/pyflowline"
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 REQUIRES_PYTHON = ">=3.6.0"
 KEYWORDS = "Earth Science"
 

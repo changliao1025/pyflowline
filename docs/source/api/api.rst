f6fd0d33b13c3b160a9e98e9848603fc12f2e88e
changliao1025 <changliao1025@outlook.com>
2023-01-15 11:15:49 -0800

    add doc ext

--- docs/source/api/api.rst
+++ docs/source/api/api.rst
@@ -2,3 +2,4 @@
 API Reference
 #############
 
+.. autodoxygenindex::
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ if read_the_docs_build:
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 #...
-extensions = [ "breathe" ]
+extensions = ['sphinx.ext.autodoc', "breathe" ]
 #...
 
 # Breathe Configuration

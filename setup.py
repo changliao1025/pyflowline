297a1289c22779736a5303913e28ea474786d2be
changliao1025 <changliao1025@outlook.com>
2024-04-16 10:59:14 -0700

    license rename

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ DESCRIPTION = \
 AUTHOR = "Chang Liao"
 AUTHOR_EMAIL = "chang.liao@pnnl.gov"
 URL = "https://github.com/changliao1025/pyflowline"
-VERSION = "0.3.6"
+VERSION = "0.3.7"
 REQUIRES_PYTHON = ">=3.8.0"
 KEYWORDS = "Earth Science"
 
